bce5798d84db633c82603e2206078478a6cb6fad
Jeho Yeon <jehoyeon617@gmail.com>
2021-09-24 00:40:22 +0900

    ListView

--- app/src/main/java/com/jeho/mysampleapp/BoardListActivity.kt
+++ app/src/main/java/com/jeho/mysampleapp/BoardListActivity.kt
@@ -3,9 +3,20 @@ package com.jeho.mysampleapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ListView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class BoardListActivity : AppCompatActivity() {
+
+    lateinit var LVAdapter : ListViewAdapter
+
+    val list = mutableListOf<Model>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -20,5 +31,41 @@ class BoardListActivity : AppCompatActivity() {
 
         }
 
+        getDate()
+
+        LVAdapter = ListViewAdapter(list)
+
+        val lv = findViewById<ListView>(R.id.lv)
+        lv.adapter = LVAdapter
+
+
     }
+
+    fun getDate() {
+
+        val database = Firebase.database
+        val myRef = database.getReference("board")
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (dataModel in dataSnapshot.children) {
+
+                    val item = dataModel.getValue(Model::class.java)
+                    Log.d("BoardListActivity", item.toString())
+                    list.add(item!!)
+
+                }
+                LVAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("BoardListActivity", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        myRef.addValueEventListener(postListener)
+
+    }
+
 }
