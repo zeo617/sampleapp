59377ab4d912991b135b3739c64ff0bfe5378f5d
Jeho Yeon <jehoyeon617@gmail.com>
2021-09-23 20:52:53 +0900

    data binding

--- app/src/main/java/com/jeho/mysampleapp/MainActivity.kt
+++ app/src/main/java/com/jeho/mysampleapp/MainActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.jeho.mysampleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
+    private lateinit var auth : FirebaseAuth
+
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -20,10 +25,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         val joinBtnClicked = findViewById<Button>(R.id.joinBtn)
         joinBtnClicked.setOnClickListener {
 
-            auth.createUserWithEmailAndPassword("abc@abc.com", "12341234")
+//          첫번째 방법
+//          val email = findViewById<EditText>(R.id.emailArea)
+//          val password = findViewById<EditText>(R.id.passwordArea)
+
+//          두번째 방법
+            val email = binding.emailArea
+            val password = binding.passwordArea
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         Toast.makeText(this, "회원가입이 완료되었습니다", Toast.LENGTH_SHORT).show()
@@ -33,6 +50,30 @@ class MainActivity : AppCompatActivity() {
                 }
 
 
+
+        }
+
+        binding.logoutBtn.setOnClickListener {
+
+            auth.signOut()
+            Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
+
+        }
+
+        binding.loginBtn.setOnClickListener {
+
+            val email = binding.emailArea
+            val password = binding.passwordArea
+
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "로그인 되었습니다", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "로그인을 진행해주세요", Toast.LENGTH_SHORT).show()
+                    }
+                }
 
         }
 
