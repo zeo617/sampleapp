4148c14e4df61644888c582b674c054beb8bbfba
Jeho Yeon <jehoyeon617@gmail.com>
2021-09-23 15:51:56 +0900

    firebase setting

--- app/src/main/java/com/jeho/mysampleapp/MainActivity.kt
+++ app/src/main/java/com/jeho/mysampleapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.jeho.mysampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val joinBtnClicked = findViewById<Button>(R.id.joinBtn)
+        joinBtnClicked.setOnClickListener {
+
+            auth.createUserWithEmailAndPassword("abc@abc.com", "12341234")
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "회원가입이 완료되었습니다", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "회원가입을 진행해주세요", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
+
+        }
+
     }
 }
